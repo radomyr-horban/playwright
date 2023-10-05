58120e20dd53f0e1b183fb253b4b77cdc7338702
Radomyr Horban <horban.radomyr@gmail.com>
2023-10-05 11:22:16 +0300

    Cleaned the branch

--- tests/pages/login.page.js
+++
@@ -1,9 +0,0 @@
-export default class LoginPage {
-  constructor(page) {
-    this.page = page
-  }
-
-  get successfulRegistrationMsg() {
-    return this.page.locator('div[id="flash_notice"]')
-  }
-}
--- tests/pages/searchResults.page.js
+++
@@ -1,9 +0,0 @@
-export default class SearchResultsPage {
-  constructor(page) {
-    this.page = page
-  }
-
-  get resultsDescription() {
-    return this.page.locator('span[class="description"]').first()
-  }
-}
